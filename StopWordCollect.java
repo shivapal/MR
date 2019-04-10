3447663add510bffde43197cb4d63186f9ace830
shivapal <shivapal@buffalo.edu>
2019-04-10 17:07:36 -0400

    Stem and stopword code added

---
+++ StopWordCollect.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+public class StopWordCollect {
+	
+	public static void readFromFile(String filepath, ArrayList<String> stop) {
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(filepath))){
+			String curLine;
+			
+			
+			while((curLine=br.readLine())!=null) {
+				if(!stop.contains(curLine)) {
+					System.out.println(curLine);
+					stop.add(curLine);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		ArrayList<String> stop = new ArrayList<String>();
+		ArrayList<String> fileps = new ArrayList<String>();
+		fileps.add("C:/Users/thear/eclipse-workspace/MR/src/stop-words/stop-words-english1.txt");
+		fileps.add("C:/Users/thear/eclipse-workspace/MR/src/stop-words/stop-words-english2.txt");
+		fileps.add("C:/Users/thear/eclipse-workspace/MR/src/stop-words/stop-words-english3-google.txt");
+		fileps.add("C:/Users/thear/eclipse-workspace/MR/src/stop-words/stop-words-english4.txt");
+		fileps.add("C:/Users/thear/eclipse-workspace/MR/src/stop-words/stop-words-english5.txt");
+
+		File file = new File("stopWordOutput.txt");
+		FileOutputStream fos = new FileOutputStream(file);
+		PrintStream ps = new PrintStream(fos);
+		System.setOut(ps);
+		for(int i=0; i<fileps.size(); i++) {
+			readFromFile(fileps.get(i), stop);
+		}
+		
+	}
+
+}
--- WordCountTest.java
+++ WordCountTest.java
@@ -1,5 +1,10 @@
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
@@ -13,16 +18,30 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCountTest {
+	private static ArrayList<String> stopwords;
+	private static Stemmer stemmer;
+	
 	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
-
+		
+		//init output key and val
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
+		
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			StringTokenizer itr = new StringTokenizer(value.toString());
+			StringTokenizer itr = new StringTokenizer(value.toString()); 
 			while (itr.hasMoreTokens()) {
-				word.set(itr.nextToken());
-				context.write(word, one);
+				String s = itr.nextToken();
+				String s1 = s.toLowerCase();
+				int ss = s1.length();
+				char[] s2 = s1.toCharArray();
+				stemmer.add(s2,ss);
+				stemmer.stem();
+				String s3 = stemmer.toString();
+				if(!stopwords.contains(s3)) {
+					word.set(s3);
+					context.write(word, one);
+				}
 			}
 		}
 	}
@@ -52,6 +71,25 @@ public class WordCountTest {
 		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		//set up stopwords
+		stopwords = new ArrayList<String>();
+		URL path = WordCountTest.class.getResource("stopWordOutput.txt");
+		File f = new File(path.getFile());
+		/*BufferedReader br = new BufferedReader(new FileReader("src/stopWordOutput.txt"))*/
+		try(BufferedReader br = new BufferedReader(new FileReader(f))){
+			String curLine;
+			while((curLine=br.readLine())!=null) {
+				stopwords.add(curLine);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//set up stemmer
+		stemmer = new Stemmer();
+		
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
